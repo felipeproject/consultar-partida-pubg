c68556173433860870f89eebd408d66770555c48
felipeproject <feelipe_andree@hotmail.com>
2024-12-25 21:33:31 -0300

    Create mapas.js

---
+++ mapas.js
@@ -0,0 +1,52 @@
+// Definindo um objeto 'mapas' com as informações de cada mapa
+// mapas.js
+const mapas = {
+    "Baltic_Main": {
+        name: "Erangel (Remastered)",
+        image: "https://wstatic-prod.pubg.com/web/live/main_d559c6b/img/9b5d63e.jpg"
+    },
+    "Chimera_Main": {
+        name: "Paramo",
+        image: "https://wstatic-prod.pubg.com/web/live/main_d559c6b/img/e74fdcd.jpg"
+    },
+    "Desert_Main": {
+        name: "Miramar",
+        image: "https://wstatic-prod.pubg.com/web/live/main_d559c6b/img/b5c3c7f.jpg"
+    },
+    "DihorOtok_Main": {
+        name: "Vikendi",
+        image: "https://wstatic-prod.pubg.com/web/live/main_d559c6b/img/1bee462.jpg"
+    },
+    "Erangel_Main": {
+        name: "Erangel",
+        image: "https://wstatic-prod.pubg.com/web/live/main_d559c6b/img/9b5d63e.jpg"
+    },
+    "Heaven_Main": {
+        name: "Haven",
+        image: "https://cebolaverde.com.br/wp-content/uploads/2020/12/PUBG-Haven.jpg"
+    },
+    "Kiki_Main": {
+        name: "Deston",
+        image: "https://wstatic-prod.pubg.com/web/live/main_d559c6b/img/b2d4a73.jpg"
+    },
+    "Range_Main": {
+        name: "Camp Jackal",
+        image: "https://pubg.wiki.gg/images/thumb/d/d2/Range_Map.jpg/950px-Range_Map.jpg"
+    },
+    "Savage_Main": {
+        name: "Sanhok",
+        image: "https://wstatic-prod.pubg.com/web/live/main_d559c6b/img/3ab566e.jpg"
+    },
+    "Summerland_Main": {
+        name: "Karakin",
+        image: "https://wstatic-prod.pubg.com/web/live/main_d559c6b/img/ce6fe02.jpg"
+    },
+    "Tiger_Main": {
+        name: "Taego",
+        image: "https://wstatic-prod.pubg.com/web/live/main_d559c6b/img/0d771c0.jpg"
+    },
+    "Neon_Main": {
+        name: "Rondo",
+        image: "https://wstatic-prod.pubg.com/web/live/main_d559c6b/img/03739c0.jpg"
+    }
+};
